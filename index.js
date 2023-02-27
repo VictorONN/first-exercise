a95fa884c88de2969538588031ffcedbbf05dc64
VictorONN <VictNO@protonmail.com>
2023-02-27 09:17:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// var x = window.prompt("are you sure?");
+// console.log(x);
+
+var todoList = document.createElement("div");
+
+var addButton = document.createElement("button");
+var removeButton = document.createElement("button");
+
+addButton.appendChild(todolist);
+removeButton.appendChild(todoList);
+
+const todolist2 = document.getElementById("list");
+
+todolist2.appendChild(todoList);
+
+// let divtag = document.getElementById("one");
+
+// divtag.addEventListener("click", () => {
+//   //how to hover over divtag
+//   console.log("clicked first task div");
+// });
+
+// divtag.addEventListener("mouseover", () => {
+//   divtag.style.backgroundColor = "red";
+// });
